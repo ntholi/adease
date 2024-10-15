1d98d952e0ae4f4842007647cd0cc11c62fe271d
Ntholi Nkhatho <ntholi@live.com>
2024-10-15 20:54:49 +0200

    export types

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import dts from 'vite-plugin-dts'; // You'll need to install this package
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/index.ts'),
