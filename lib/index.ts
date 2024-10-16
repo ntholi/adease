5337248887b440f0800a018152886c43a6e3e28e
Ntholi Nkhatho <ntholi@live.com>
2024-10-16 08:25:22 +0200

    disable exporting types for now

--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,3 @@
+import '@mantine/core/styles.css';
 export { default as Button } from './Button/Button';
 export { default as FieldView } from './components/FieldView';
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
-import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    dts({
-      insertTypesEntry: true,
-    }),
-  ],
+  plugins: [react()],
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/index.ts'),
