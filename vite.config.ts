2e62b5b680094b3166b09b36296ef2740e5538df
Ntholi Nkhatho <ntholi@live.com>
2024-10-16 05:26:23 +0200

    format code

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
-import dts from 'vite-plugin-dts'; // You'll need to install this package
+import dts from 'vite-plugin-dts';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
