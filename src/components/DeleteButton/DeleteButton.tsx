1be1ac471ea2da523a2b31d9d0d6373fabd45990
Ntholi Nkhatho <ntholi@live.com>
2024-10-16 17:36:28 +0200

    improve handleDelete

--- src/components/DeleteButton/DeleteButton.tsx
+++ src/components/DeleteButton/DeleteButton.tsx
@@ -5,37 +5,60 @@ import {
   MantineProvider,
   Text,
 } from '@mantine/core';
-import { modals } from '@mantine/modals';
+import { modals, ModalsProvider } from '@mantine/modals';
 import { TrashIcon } from 'lucide-react';
 import { useState } from 'react';
 
 export interface DeleteButtonProps extends ActionIconProps {
   action: (id: string | number) => Promise<void>;
   id: number | string;
+  message?: string;
 }
 
-export function DeleteButton({ action, id, ...props }: DeleteButtonProps) {
+export function DeleteButton({
+  action,
+  id,
+  message,
+  ...props
+}: DeleteButtonProps) {
   const [loading, setLoading] = useState(false);
 
   const openModal = () =>
     modals.openConfirmModal({
       title: 'Confirm Delete',
-      children: <Text size='sm'>Are you sure you want to delete this</Text>,
+      children: (
+        <Text size='sm'>
+          {message || 'Are you sure you want to delete this'}
+        </Text>
+      ),
       labels: { confirm: 'Delete', cancel: 'Cancel' },
       confirmProps: { color: 'red' },
       onCancel: () => console.log('Cancel'),
       onConfirm: () => handleDelete(),
     });
 
-  function handleDelete() {
+  async function handleDelete() {
     setLoading(true);
-    action(id).finally(() => setLoading(false));
+    try {
+      await action(id);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
   }
   return (
     <MantineProvider>
-      <ActionIcon color='red' loading={loading} onClick={openModal} {...props}>
-        <TrashIcon size={'1rem'} />
-      </ActionIcon>
+      <ModalsProvider>
+        <ActionIcon
+          color='red'
+          loading={loading}
+          onClick={openModal}
+          {...props}
+        >
+          <TrashIcon size={'1rem'} />
+        </ActionIcon>
+      </ModalsProvider>
     </MantineProvider>
   );
 }
