cd622983322478077232c77e41da7d9e6bc37ceb
Ntholi Nkhatho <ntholi@live.com>
2024-10-16 08:12:05 +0200

    setup mantine and add FieldView component

---
+++ lib/components/FieldView.tsx
@@ -0,0 +1,33 @@
+import { Box, BoxProps, Divider, MantineProvider, Text } from '@mantine/core';
+import React from 'react';
+
+type Props = {
+  label: string;
+  children: React.ReactNode;
+} & BoxProps;
+
+export default function FieldView({ label, children, ...props }: Props) {
+  return (
+    <MantineProvider>
+      <Box {...props}>
+        {children ? (
+          <>
+            {React.isValidElement(children) ? (
+              children
+            ) : (
+              <Text size='sm' fw={500}>
+                {children}
+              </Text>
+            )}
+          </>
+        ) : (
+          <Text size='sm'>Empty</Text>
+        )}
+        <Text size='sm' c='dimmed'>
+          {label}
+        </Text>
+        <Divider />
+      </Box>
+    </MantineProvider>
+  );
+}
